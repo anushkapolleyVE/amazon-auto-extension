11cd7de51b1afd8d513294b5639c96b7d7b7209e
anushkapolleyVE <anushkapolley@virtualemployee.com>
2026-07-31 19:30:25 +0530

    fixed bcrypt max length bug

--- backend/app/schemas/auth.py
+++ backend/app/schemas/auth.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, EmailStr, Field
 class RegisterRequest(BaseModel):
     name: str = Field(..., min_length=2, max_length=100)
     email: EmailStr
-    password: str = Field(..., min_length=8)
+    password: str = Field(..., min_length=8, max_length=72)
 
 
 class LoginRequest(BaseModel):
